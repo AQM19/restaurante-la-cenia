9eb6a5f2308374127019782859932da09f006582
AQM19 <quintanalaaron@gmail.com>
2024-12-02 10:17:09 +0100

    Fix: Corrección de la página

--- src/app/[locale]/(inner)/menu/page.tsx
+++ src/app/[locale]/(inner)/menu/page.tsx
@@ -2,7 +2,7 @@ import { MenuConfig } from '@/core/config/menu.config'
 import { useTranslations } from 'next-intl'
 import React from 'react'
 
-const page = () => {
+const MenuCardPage = () => {
 
     const t = useTranslations('Menu')
 
@@ -48,4 +48,4 @@ const page = () => {
     )
 }
 
-export default page
+export default MenuCardPage
